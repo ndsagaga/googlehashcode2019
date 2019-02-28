1ddb10c5e62bca8a92dc30b48bd2c22ac102e582
Ariana Daka <ad2104@rit.edu>
2019-02-28 14:48:26 -0500

    Added shuffle function

--- Main.py
+++ Main.py
@@ -2,11 +2,16 @@ import Read
 import Score
 import Slide
 import Print
+import random
 
 photos = []
 
+def shuffling(slides):
+    random.shuffle(slides)
+    return slides
+
 def main():
-    photos = Read.read("data/a_example.txt")
+    photos = Read.read("data/b_lovely_landscapes.txt")
     Slide.photo_array = photos
     slides = []
     slideV = []
@@ -21,6 +26,13 @@ def main():
 
     print(Score.score(slides))
     Print.print_slides(slides)
+
+    population = [slides]
+
+    for i in range(50):
+        population.append(shuffling(slides))
+        print(Score.score(population[-1]))
+
 
 if __name__ == '__main__':
     main()
